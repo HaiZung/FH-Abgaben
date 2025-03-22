6ba0a125dbd8e7388bddc32a124a12a94fe8eda6
Your Name <leon@air-von-leon-3.speedport.ip>
2025-03-22 11:00:38 +0100

    Projektbeschreibung

---
+++ script.js
@@ -0,0 +1,25 @@
+const zugspitze = {
+  name: "Zugspitze",
+  hoehe: 2962,
+  land: "Deutschland",
+  bild: "Zugspitze.jpeg",
+  beschreibung: "Zugspitze: 2962m"
+};
+
+const schneefernerkopf = {
+  name: "Schneefernerkopf",
+  hoehe: 2875,
+  land: "Deutschland",
+  bild: "Schneefernerkopf_1.jpg",
+  beschreibung: "Schneefernerkopf: 2875m"
+};
+
+const hochwanner = {
+  name: "Hochwanner",
+  hoehe: 2744,
+  land: "Deutschland",
+  bild: "Hochwanner.jpg",
+  beschreibung: "Hochwanner: 2744",
+};
+
+
